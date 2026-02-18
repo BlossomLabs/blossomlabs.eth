a2062b90c36733685a563acca8486644493f495f
Sem <931684+sembrestels@users.noreply.github.com>
2026-02-18 23:23:28 +0100

    Migrate to astro

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://blossom.software',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
